cd3b90770b6cafd6c4bedae61e091bf878766b46
dongseobkim <seob6615@gmail.com>
2021-10-27 01:21:30 +0900

    02 문자열뒤집기, 03 로그파일재정렬

--- 3_String_Manipulation_02.py
+++ 3_String_Manipulation_02.py
@@ -28,3 +28,45 @@ class ans2():
 
 ans2 = ans2()
 print(ans2.reverseString(["h","e","l","l","o"]))
+
+# 03 로그 파일 재정렬
+""" 로그를 재정렬하라. 기준은 다음과 같다.
+1. 로그의 가장 앞 부분은 식별자다.
+2 .문자로 구성된 로그가 숫자 로그보다 앞에 온다.
+3. 식별자는 순서에 영향을 끼치지 않지만, 문자가 동일할 경우 식별자 순으로 한다.
+4. 숫자 로그는 입력 순서대로 한다.
+예시) logs = ['dig1 8 1 5 1', 'let1 art can', 'dig2 3 6', 'let2 own kit dig', 'let3 art zero']
+print out -> ['let1 art can', 'let3 art zero', 'let2 own kit dig', 'dig1 8 1 5 1', 'dig2 3 6']
+"""
+class reorder_log_1():
+    
+    def reorder_log(self, logs: list) -> list:
+        letters = []
+        digits = []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        # 2개의 키를 람다 표현식으로 정렬
+        # sort 함수 -> key 매개변수 활용하여 key값을 기준으로 정렬되고 오름차순으 디폴트.
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
+
+logs = ['dig1 8 1 5 1', 'let1 art can', 'dig2 3 6', 'let2 own kit dig', 'let3 art zero']
+reorder_log = reorder_log_1()
+print(reorder_log.reorder_log(logs))
+
+
+
+
+
+
+
+
+
+
+
+
